feabb9aaed1cfd7d558d5252ef4a9684fb28568b
Daniel_Alvarado <danielalvarado1498@gmail.com>
2022-01-11 19:55:34 -0600

    Issues fixed

--- src/components/CreatePost/index.js
+++ src/components/CreatePost/index.js
@@ -2,6 +2,7 @@
 import { react, useState, useEffect } from "react";
 import Select from "react-select";
 import styles from "./styles.css";
+import { Link } from "react-router-dom";
 
 const getUsers = async () => {
   const response = await fetch("http://localhost:8080/users");
@@ -126,66 +127,6 @@ const CreatePost = () => {
 
   return (
     <div>
-      {/* <header>
-        <div className="container-fluid c-fluid newPostBarBackground">
-          <nav className="navbar navbar-expand-lg navbar_bar">
-            <div className="container container_bar editPostBar">
-              <a className="navbar-logo" href="../index.html">
-                <svg
-                  width="50"
-                  height="40"
-                  viewBox="0 0 50 40"
-                  fill="#fff"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect
-                    width="50"
-                    height="40"
-                    rx="3"
-                    style={{ fill: "#000" }}
-                  ></rect>
-                  <path
-                    d="M19.099 23.508c0 1.31-.423 2.388-1.27 3.234-.838.839-1.942 1.258-3.312 1.258h-4.403V12.277h4.492c1.31 0 2.385.423 3.224 1.27.846.838 1.269 1.912 1.269 3.223v6.738zm-2.808 0V16.77c0-.562-.187-.981-.562-1.258-.374-.285-.748-.427-1.122-.427h-1.685v10.107h1.684c.375 0 .75-.138 1.123-.415.375-.285.562-.708.562-1.27zM28.185 28h-5.896c-.562 0-1.03-.187-1.404-.561-.375-.375-.562-.843-.562-1.404V14.243c0-.562.187-1.03.562-1.404.374-.375.842-.562 1.404-.562h5.896v2.808H23.13v3.65h3.088v2.808h-3.088v3.65h5.054V28zm7.12 0c-.936 0-1.684-.655-2.246-1.965l-3.65-13.758h3.089l2.807 10.804 2.808-10.804H41.2l-3.65 13.758C36.99 27.345 36.241 28 35.305 28z"
-                    style={{ fill: "var(--base-inverted)" }}
-                  ></path>
-                </svg>
-              </a>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div
-                className="collapse navbar-collapse"
-                id="navbarSupportedContent"
-              >
-                <span className="create">Create Post</span>
-                <ul className="navbar-nav mb-2 mb-lg-0 navbar-nav_bar">
-                  <li className="nav-item createFilter">
-                    <a className="item-link filterElement" href="#">
-                      Edit
-                    </a>
-                    <span className="item-line"></span>
-                  </li>
-                  <li className="nav-item createFilter">
-                    <a className="item-link filterElement2" href="#">
-                      Preview
-                    </a>
-                    <span className="item-line"></span>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </header> */}
-
       <div className="container">
         <div className="row">
           <div className="col-g-7 col-md-7 col-s-12 editNewPost">
@@ -299,15 +240,17 @@ const CreatePost = () => {
       <div className="container ">
         <div className="row">
           <div className="col-g-7 col-md-7 col-s-12 finalbuttons">
-            <button
-              className="submitbutton"
-              id="saveButton"
-              onClick={() => handleSubmit()}
-            >
-              <a className="styleDeleter" href="../index.html">
-                Publish
-              </a>
-            </button>
+            <Link to="/">
+              <button
+                className="submitbutton"
+                id="saveButton"
+                onClick={() => handleSubmit()}
+              >
+                <a className="styleDeleter" href="../index.html">
+                  Publish
+                </a>
+              </button>
+            </Link>
             <button className="saveDraft">Save Draft</button>
           </div>
         </div>
